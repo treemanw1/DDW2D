7b895af0db802df914369da2a11055938fd04456
milselarch <milselarch@gmail.com>
2021-11-22 18:33:10 +0800

    shift analysis

--- charles/shift_analysis.py
+++ charles/shift_analysis.py
@@ -1,15 +1,9 @@
+from tqdm.auto import tqdm
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-df = pd.read_csv('datasets/owid-covid-data.csv')
-df = df[df['continent'].notna()]
-df['new_cases'] = df['new_cases'].fillna(0)
-df['new_deaths'] = df['new_deaths'].fillna(0)
-
-iso_codes = df['iso_code'].to_numpy()
-iso_codes = np.unique(iso_codes)
 
 
 def assert_continuity(sub_df):
@@ -20,9 +14,46 @@ def assert_continuity(sub_df):
     assert max(day_diffs) < 2
 
 
-def covariances(shift_start=0, shift_end=20):
-    for shift in range(shift_start, shift_end):
+def covariances(
+    base_df, shift_start=0, shift_end=50,
+    cases_column='new_cases_smoothed',
+    deaths_column='new_deaths_smoothed',
+    stride=1
+):
+    iso_codes = base_df['iso_code'].to_numpy()
+    iso_codes = np.unique(iso_codes)
+    corr_mapping = {}
+
+    for shift in tqdm(range(shift_start, shift_end)):
+        all_cases, all_deaths = [], []
+
         for iso_code in iso_codes:
-            iso_df = df[df['iso_code'] == iso_code]
-            assert_continuity(iso_df)
+            iso_df = base_df[base_df['iso_code'] == iso_code]
+            # assert_continuity(iso_df)
+
+            cases = iso_df[cases_column].fillna(0).to_numpy()
+            deaths = iso_df[deaths_column].fillna(0).to_numpy()
+
+            length = len(cases)
+            cases = cases[:length-shift]
+            deaths = deaths[shift:]
+
+            all_cases.append(cases)
+            all_deaths.append(deaths)
+
+        all_cases = np.concatenate(all_cases, axis=0)[::stride]
+        all_deaths = np.concatenate(all_deaths, axis=0)[::stride]
+        correlation = np.corrcoef(all_cases, all_deaths)
+        # print(f'CORR {shift} {correlation}')
+        corr_mapping[shift] = correlation
+
+    return corr_mapping
+
+
+if __name__ == '__main__':
+    df = pd.read_csv('datasets/owid-covid-data.csv')
+    df = df[df['continent'].notna()]
+    df['new_cases'] = df['new_cases'].fillna(0)
+    df['new_deaths'] = df['new_deaths'].fillna(0)
+    covariances(base_df=df)
 
